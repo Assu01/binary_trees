d02a3aa0676108c33e4fd483f38fb00596162913
Asrat Asmelash <asratasse@gmail.com>
2022-08-25 21:28:20 +0300

    0x09. Web infrastructure design

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,30 +1,72 @@
 #include "binary_trees.h"
-#include<stdlib.h>
+
 /**
- * heap_to_sorted_array - converts a Binary Max Heap to a sorted array of
- * integers
- * @heap: pointer to the root node of the heap to convert
- * @size: address to store the size of the array
- * Return: sorted array of integers
+ * _realloc - Reallocates a memory block
+ * @ptr: The pointer to the previous memory block
+ * @old_size: The size of the old memory block
+ * @new_size: The size of the new memory block
+ *
+ * Return: The pointer to the new memory block otherwise NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	unsigned int min_size = old_size < new_size ? old_size : new_size;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		new_ptr = malloc(new_size);
+		if (new_ptr != NULL)
+		{
+			for (i = 0; i < min_size; i++)
+				*((char *)new_ptr + i) = *((char *)ptr + i);
+			free(ptr);
+			return (new_ptr);
+		}
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		return (new_ptr);
+	}
+}
+
+/**
+ * heap_to_sorted_array - Creates a sorted array from a max binary heap tree.
+ * @heap: A pointer to the max binary heap.
+ * @size: A pointer to the resulting array's size value.
+ *
+ * Return: A pointer to the array, otherwise NULL.
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	int *array;
-	int extract, i = 0;
-	size_t heap_size;
+	int *array = NULL;
+	heap_t *root;
+	int val;
+	size_t n = 0;
 
-	if (!heap)
-		return (NULL);
-	heap_size = binary_tree_size(heap);
-	*size = heap_size;
-	array = malloc(heap_size * sizeof(int));
-	if (!array)
-		return (NULL);
-	while (heap)
+	if (heap != NULL)
 	{
-		extract = heap_extract(&heap);
-		array[i] = extract;
-		i++;
+		root = heap;
+		while (root != NULL)
+		{
+			val = heap_extract(&root);
+			array = _realloc(array, sizeof(int) * n, sizeof(int) * (n + 1));
+			*(array + n) = val;
+			n++;
+		}
 	}
+	if (size != NULL)
+		*size = n;
 	return (array);
 }
